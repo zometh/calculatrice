e95611d22b52b206ca7d7cdcd43e560d84c11cca
zometh <metzodiop45@gmail.com>
2024-11-29 23:38:41 +0000

    second

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
-let result = document.getElementById('header');
-result.addEventListener("click", function (){
-    result.innerHTML = "kkk";
-})
+var result = document.getElementById('header');
+var res = 0;
+document.getElementById("equal")
+    .addEventListener("click", () => {
+        result.innerText = res;
+    })
+
+function updateResultValue(newValue){
+    const currentValue = result.textContent;
+    result.innerText = currentValue + " " + newValue;
+    res += parseInt(newValue);
+}
 const items = document.querySelectorAll("#item");
 items.forEach(item => item.addEventListener("click", function (){
     let value = item.textContent;
-    let content = result.textContent;
-    if(content == "0"){
-        result.innerText = value;  
-    }
-    else{
-        result.innerHTML = content + " " + value;
-    }
+    updateResultValue(value);
 }))
