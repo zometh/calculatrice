3a2bdcb33e48d416647a23ae91cdd4cbe899ffe3
zometh <metzodiop45@gmail.com>
2024-12-02 14:37:30 +0000

    work done

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
+var display =  document.getElementById('result');
 
-var result = document.getElementById('header');
-var res = 0;
-document.getElementById("equal")
-    .addEventListener("click", () => {
-        result.innerText = res;
-    })
+var result = "";
 
-function updateResultValue(newValue){
-    const currentValue = result.textContent;
-    result.innerText = currentValue + " " + newValue;
-    res += parseInt(newValue);
+document.getElementById('equal').addEventListener(
+    "click", () => {
+        
+        display.innerHTML = eval(result);
+        //clear();
+        
+        
+    }
+);
+document.getElementById('clear').addEventListener(
+    "click", () => {
+        display.textContent = 0;
+        result = "";
+    }
+);
+function clear(){
+    document.getElementById('result').textContent = 0;
+    
+    result = "";
 }
-const items = document.querySelectorAll("#item");
-items.forEach(item => item.addEventListener("click", function (){
-    let value = item.textContent;
-    updateResultValue(value);
-}))
+const values = document.querySelectorAll('#value');
+const operators = document.querySelectorAll('#operator');
+operators.forEach(operator => operator.addEventListener(
+    'click', () =>{
+        let op = getOperator(operator.textContent);
+        display.textContent += op;
+        result =  display.textContent;
+      
+    }
+));
+values.forEach(value => value.addEventListener('click', function (){
+    if(display.textContent == "0"){
+        display.textContent = '';
+    }
+    display.textContent = display.textContent + value.textContent;
+    result =  display.textContent;
+}));
+function getOperator(operator){
+    switch (operator) {
+        case "+":
+            return "+";
+        case "-":
+            return "-";
+        case "÷":
+            return '/';
+        
+    
+        default:
+            return "*";
+    }
+}
